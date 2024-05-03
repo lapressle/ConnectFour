44f27269b1bdc47eef182960fd903ee77b41b369
lapressle <112499468+lapressle@users.noreply.github.com>
2024-05-03 01:22:50 -0400

    Update game_win tests to accommodate losing option

--- lib/connectfour.rb
+++ lib/connectfour.rb
@@ -1,12 +1,16 @@
 # frozen_string_literal: true
 
 class Game
-  def initialize()
-    @game_board = Game_Board.new()
+  def initialize(game_board = Game_Board.new())
+    @game_board = game_board
   end
 
   def win_game?
-    true
+    if @game_board.complete_row? || @game_board.complete_column? || @game_board.complete_diagonal?
+      true
+    else
+      false
+    end
   end
 end
 
--- spec/connectfour_spec.rb
+++ spec/connectfour_spec.rb
@@ -3,8 +3,8 @@
 require_relative '../lib/connectfour'
 
 describe Game do
-  subject(:example_game) { described_class.new() }
   describe '#win_game?' do
+    subject(:example_game) { described_class.new(game_board) }
     let(:game_board) { instance_double(Game_Board) }
     context 'when the board has a complete row' do
       it 'returns true' do
@@ -15,6 +15,7 @@ describe Game do
 
     context 'when the board has a complete column' do
       it 'returns true' do
+        allow(game_board).to receive(:complete_row?).and_return(false)
         allow(game_board).to receive(:complete_column?).and_return(true)
         expect(example_game.win_game?).to be(true)
       end
@@ -22,8 +23,19 @@ describe Game do
 
     context 'when the board has a complete diagonal' do
       it 'returns true' do
+        allow(game_board).to receive(:complete_row?).and_return(false)
+        allow(game_board).to receive(:complete_column?).and_return(false)
         allow(game_board).to receive(:complete_diagonal?).and_return(true)
         expect(example_game.win_game?).to be(true)
+      end
+    end
+
+    context 'when the board no win conditions are satisfied' do
+      it 'returns true' do
+        allow(game_board).to receive(:complete_column?).and_return(false)
+        allow(game_board).to receive(:complete_diagonal?).and_return(false)
+        allow(game_board).to receive(:complete_row?).and_return(false)
+        expect(example_game.win_game?).to be(false)
       end
     end
   end
