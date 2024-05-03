83cf1a0e9b6a423be1089653aeb6ff610323d6cb
lapressle <112499468+lapressle@users.noreply.github.com>
2024-05-03 00:55:46 -0400

    Complete first test for game_win?

--- lib/connectfour.rb
+++ lib/connectfour.rb
@@ -1,1 +1,15 @@
 # frozen_string_literal: true
+
+class Game
+  def initialize
+  end
+
+  def win_game?
+    true
+  end
+end
+
+class Game_Board 
+  def complete_row?
+  end
+end
--- spec/connectfour_spec.rb
+++ spec/connectfour_spec.rb
@@ -1,3 +1,16 @@
 # frozen_string_literal: true
 
 require_relative '../lib/connectfour'
+
+describe Game do
+  subject(:example_game) { described_class.new() }
+  describe '#win_game?' do
+    let(:game_board) { instance_double(Game_Board) }
+    context 'when the board has a complete row' do
+      it 'returns true' do
+        allow(game_board).to receive(:complete_row?).and_return(true)
+        expect(example_game.win_game?).to be(true)
+      end
+    end
+  end
+end
